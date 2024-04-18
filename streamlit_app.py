8d3721f05a599c8bf186032980c1392c1dcb4c87
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-04-18 22:31:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+import plost
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+# Row A
+st.markdown('### Metrics')
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
