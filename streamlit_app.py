0d31a099e556c03546278a35d2db9386d54bc116
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-04-19 15:22:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,6 @@ st.title('Best Dash💖')
 st.write('This is gonna be XAI dashboard')
 
 # Row A
-st.markdown('### Metrics')
-
 vars = [['education', 'Уровень образования сотрудника'], ['gender', 'Пол сотрудника'], 
         ['no_of_trainings', 'Кол-во тренингов, которые прошел сотрудник за последний год'],
         ['avg_training_score', 'Средняя оценка за пройденные тренинги'], ['age', 'Возраст сотрудника'], 
@@ -23,7 +21,16 @@ vars = [['education', 'Уровень образования сотрудник�
         ['department_', 'Отдел, в котором работает сотрудник']]
 vars_df = pd.DataFrame(vars, columns=['Переменная', 'Описание'])
 
-st.table(vars_df)
+explainer = shap.Explainer(modelGB)
+shap_values = explainer.shap_values(df)
+
+st.markdown('### Metrics')
+col1, col2 = st.columns(2)
+col1.table(vars_df)
+
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, df, plot_type='bar')
+col2.pyplot(bbox_inches='tight')
 
 # Row B
 df = pd.read_csv('https://raw.githubusercontent.com/cekatirina/data/master/X_test.csv')
@@ -34,12 +41,3 @@ prediction_proba = modelGB.predict_proba(df)
 
 st.subheader('Prediction')
 st.write(prediction_proba[10])
-
-# Row C
-explainer = shap.Explainer(modelGB)
-shap_values = explainer.shap_values(df)
-
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, df, plot_type='bar')
-st.pyplot(bbox_inches='tight')
