6f5784df95eda11f8772f720535e5ee6462dc299
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-04-28 11:06:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ with tab1:
                 st.markdown('### Важность предикторов')
                 plt.title('Feature importance based on SHAP values')
                 shap.summary_plot(shap_values, df, plot_type='bar')
-                plt.subplots(figsize=(400, 200))
                 st.pyplot()
                 
         # Row B
