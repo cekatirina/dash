245ba26b4cf1a5bf495c930e023ca67479f6e956
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-04-28 11:37:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,8 @@ with tab1:
                 st.pyplot()
                 
         # Row B
+        st.markdown('### Важность предикторов')
+        plt.title('Feature contribution based on SHAP values')
         shap.dependence_plot("avg_training_score", shap_values, df,
                     feature_names=df.columns, interaction_index="gender")
         st.pyplot()
