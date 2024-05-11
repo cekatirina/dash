2015ef72ad909e2edd2cfeaedc11813133640950
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-05-11 10:46:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ import sklearn
 import shap
 import matplotlib.pyplot as plt
 from streamlit_gsheets import GSheetsConnection
+import ssl
+import urllib3
+import requests
+import io
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -160,8 +164,35 @@ with tab3:
                                 }
                             ]
                         )
-                        # Add the new vendor data to the existing data
-                        existing_data = conn.read(spreadsheet=url, worksheet="Answers", usecols=list(range(10)))
+                        # Add the new vendor data to the existing data                      
+                        
+                        class CustomHttpAdapter(requests.adapters.HTTPAdapter):
+                            # "Transport adapter" that allows us to use custom ssl_context.
+                        
+                            def __init__(self, ssl_context=None, **kwargs):
+                                self.ssl_context = ssl_context
+                                super().__init__(**kwargs)
+                        
+                            def init_poolmanager(self, connections, maxsize, block=False):
+                                self.poolmanager = urllib3.poolmanager.PoolManager(
+                                    num_pools=connections,
+                                    maxsize=maxsize,
+                                    block=block,
+                                    ssl_context=self.ssl_context,
+                                )
+                        
+                        
+                        def get_legacy_session():
+                            ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+                            ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+                            session = requests.session()
+                            session.mount("https://", CustomHttpAdapter(ctx))
+                            return session
+                        
+                        res = get_legacy_session().get(url)                        
+                        st.write(df)
+                
+                        existing_data = conn.read(spreadsheet=res, worksheet="Answers", usecols=list(range(10)))
                         existing_data = existing_data.dropna(how="all")
                         updated_df = pd.concat([existing_data, answers], ignore_index=True)
 
